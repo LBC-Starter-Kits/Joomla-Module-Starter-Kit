366dda3a5468692d8c3c4757e93644555cd285eb
Luis BC <luinux81@gmail.com>
2020-10-08 12:55:22 +0200

    Reset

--- mod_moduloEjemplo.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined("_JEXEC") or die;
-
-//Cargar fichero helper.php
-require_once __DIR__ . "/helper.php";
-
-//Cargar la vista por defecto del módulo
-require JModuleHelper::getLayoutPath("mod_moduloEjemplo");
-?>
